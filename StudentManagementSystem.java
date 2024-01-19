4d039bae04bf927a1c80f1df460ea1082cdd4350
tanzil112 <114287025+tanzil112@users.noreply.github.com>
2024-01-19 23:26:16 +0530

    Update StudentManagementSystem.java

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -20,7 +20,7 @@ System.out.println("Student Details:");
 System.out.println("Name: " + name);
 System.out.println("Age: " + age);
 System.out.println("Grade: " + grade);
-displayRemarks(); // Display custom remarks based on grade
+displayRemarks(); 
 System.out.println("--------------");
 }
 
@@ -49,33 +49,28 @@ int studentCount;
 System.out.print("Enter the number of students: ");
 studentCount = scanner.nextInt();
 
-// Create an array to store students
 Student[] students = new Student[studentCount];
 
-// Input student details
 for (int i = 0; i < studentCount; i++) {
 System.out.println("\nEnter details for Student " + (i + 1) + ":");
 System.out.print("Name: ");
-scanner.nextLine(); // Consume the newline character
+scanner.nextLine(); 
 String name = scanner.nextLine();
 System.out.print("Age: ");
 int age = scanner.nextInt();
 System.out.print("Grade: ");
 String grade = scanner.next();
 
-// Create a new student object and add it to the array
 students[i] = new Student(name, age, grade);
 }
 
-// Display details of all students
 System.out.println("\nDetails of all students:");
 for (Student student : students) {
 student.displayDetails();
 }
 
-// Search for a student by name
 System.out.print("\nEnter the name of the student to search: ");
-scanner.nextLine(); // Consume the newline character
+scanner.nextLine(); 
 String searchName = scanner.nextLine();
 boolean found = false;
 
